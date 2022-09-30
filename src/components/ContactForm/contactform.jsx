2566f1e20bacbfb60dd2ed1edbdd5a58de7049ab
Haik <55463651+salampego@users.noreply.github.com>
2022-10-01 02:41:37 +0300

    fix

--- src/components/ContactForm/contactform.jsx
+++ src/components/ContactForm/contactform.jsx
@@ -43,6 +43,7 @@ export function ContactForm({ submit }) {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           onChange={inputCorrect}
+          value={name}
           required
         />
       </label>
@@ -54,6 +55,7 @@ export function ContactForm({ submit }) {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           onChange={inputCorrect}
+          value={number}
           required
         />
       </label>
